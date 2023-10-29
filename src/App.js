74a77ad063049762ef96498e8b39102adb6117c4
guhesse <hesse.gustavo@gmail.com>
2023-10-29 18:27:33 -0300

    SSL and SL with errors

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import HeaderSelector from "./HeaderSelector.js";
+import SubjectLineSelector from './SubjectLineSelector.js';
 import { Theme } from "@swc-react/theme";
 const { core, app } = require('photoshop');
 const { storage } = require('uxp');
 const { batchPlay } = require('photoshop').action;
 
+
 function App() {
+
+  //Função de selecionar o Header
   const [selectedHeader, setSelectedHeader] = useState(null);
 
   const handleHeaderSelect = async (header) => {
-    console.log('Header selecionado:', header);
     const headerFilePath = `assets/${header}.psd`;
-    console.log('Caminho do arquivo:', headerFilePath);
     const fs = storage.localFileSystem;
 
     try {
@@ -57,9 +59,9 @@ function App() {
           pastedGroup.translate(offsetX, offsetY);
           await activeDocument.save();
 
-          console.log('Cabeçalho inserido com sucesso!');
+          console.log('Header inserido com sucesso!');
         } catch (error) {
-          console.error('Erro ao inserir o cabeçalho:', error);
+          console.error('Erro ao inserir o Header:', error);
         }
       };
 
@@ -70,12 +72,152 @@ function App() {
 
       await core.executeAsModal(targetFunction, options);
     } catch (error) {
-      console.error('Erro ao encontrar o arquivo:', error);
+      console.error('Erro ao encontrar o arquivo do Header:', error);
     }
   };
+  // Fim de função de selecionar o Header
+
+  //Função de moficar e importar o SSL
+
+  const [subjectLineValues, setSubjectLineValues] = useState(null);
+
+  const handleSubjectLineChange = (values) => {
+    setSubjectLineValues(values);
+  };
+
+  const slValue = subjectLineValues?.slValue;
+  const sslValue = subjectLineValues?.sslValue;
+
+  const sslSelect = async (ssl) => {
+    const sslFilePath = `assets/NON-LAYOUT_CONTENT.psd`;
+    const fs = storage.localFileSystem;
+
+    try {
+      const pluginDir = await fs.getPluginFolder();
+      const fileEntry = await pluginDir.getEntry(sslFilePath);
+
+      const targetFunction = async (executionContext) => {
+        try {
+          await app.open(fileEntry);
+          const batchSSLChangeAndCopy = [
+            {
+              _obj: "select",
+              _target: [
+                {
+                  _ref: "layer",
+                  _name: "SL"
+                }
+              ],
+              makeVisible: false,
+              layerID: [
+                2125
+              ],
+              _options: {
+                dialogOptions: "dontDisplay"
+              }
+            },
+
+            {
+              _obj: "set",
+              _target: [
+                {
+                  _ref: "textLayer",
+                  _enum: "ordinal",
+                  _value: "targetEnum"
+                }
+              ],
+              to: {
+                _obj: "textLayer",
+                textKey: `SL PT :  ${getSubjectLineValues().slValue}`,
+              }
+            },
+            {
+              _obj: "select",
+              _target: [
+                {
+                  _ref: "layer",
+                  _name: "SSL"
+                }
+              ],
+              makeVisible: false,
+              layerID: [
+                2125
+              ],
+              _options: {
+                dialogOptions: "dontDisplay"
+              }
+            },
+
+            {
+              _obj: "set",
+              _target: [
+                {
+                  _ref: "textLayer",
+                  _enum: "ordinal",
+                  _value: "targetEnum"
+                }
+              ],
+              to: {
+                _obj: "textLayer",
+                textKey: `SSL :  ${getSubjectLineValues().slValue}`,
+              }
+            },
+            {
+              _obj: "selectAllLayers",
+              _target: [
+                {
+                  _ref: "layer",
+                  _enum: "ordinal",
+                  _value: "targetEnum"
+                }
+              ],
+              _options: {
+                dialogOptions: "dontDisplay"
+              }
+            },
+            {
+              _obj: "copyEvent",
+              _options: {
+                dialogOptions: "dontDisplay"
+              }
+            }
+          ];
+
+          await batchPlay(batchSSLChangeAndCopy, {});
+          await app.activeDocument.save();
+          await app.activeDocument.close();
+
+          const activeDocument = app.activeDocument;
+          await activeDocument.paste();
+          const pastedGroup = activeDocument.layers[activeDocument.layers.length - 1];
+          const docWidth = activeDocument.width;
+          const docHeight = activeDocument.height;
+          const offsetX = ((docWidth - docWidth) - (docWidth / 2) + 325);
+          const offsetY = ((docHeight - docHeight) - (docHeight / 2) + 30);
+          pastedGroup.translate(offsetX, offsetY);
+          await activeDocument.save();
+
+          console.log('SSL inserido com sucesso!');
+        } catch (error) {
+          console.error('Erro ao inserir SSL:', error);
+        }
+      };
+
+      const options = {
+        commandName: 'Inserir SSL',
+        interactive: true,
+      };
+
+      await core.executeAsModal(targetFunction, options);
+    } catch (error) {
+      console.error('Erro ao encontrar o arquivo de SSL:', error);
+    }
+  };
+  // Fim de função de modificar e importar o SSL
 
   const handleMontarLayoutClick = async () => {
     try {
+      await sslSelect();
       await handleHeaderSelect(selectedHeader);
       // Outras funções que você deseja executar podem ser chamadas aqui
       // await outraFuncao();
@@ -89,7 +231,10 @@ function App() {
   return (
     <div width="100" style={{ paddingLeft: "15px", paddingTop: "10px", width: "100vw" }}>
       <Theme theme="spectrum" scale="medium" color="light">
-        <HeaderSelector handleHeaderSelect={setSelectedHeader}></HeaderSelector>
+        <SubjectLineSelector onSubjectLineChange={handleSubjectLineChange} />
+        <p>SL: {slValue}</p>
+        <p>SSL: {sslValue}</p>
+        <HeaderSelector handleHeaderSelect={setSelectedHeader} />
         <sp-button style={{ marginTop: "8px" }} onClick={handleMontarLayoutClick}>
           Montar layout
         </sp-button>
--- src/SubjectLineSelector.js
+++ src/SubjectLineSelector.js
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SubjectLineSelector() {
+    const [slValue, setSlValue] = useState(""); // Estado para armazenar o valor do campo SL
+    const [sslValue, setSslValue] = useState(""); // Estado para armazenar o valor do campo SSL
+
+    const handleSlChange = (event) => {
+        setSlValue(event.target.value);
+        props.onSubjectLineChange({
+            slValue,
+            sslValue,
+        });
+    };
+    console.log("valor sl:", slValue)
+
+    const handleSslChange = (event) => {
+        setSslValue(event.target.value);
+        props.onSubjectLineChange({
+            slValue,
+            sslValue,
+        });
+    };
+    console.log("valor ssl:", sslValue)
+
+    const getSubjectLineValues = () => {
+        return {
+            slValue,
+            sslValue,
+        };
+    };
+
     return (
-        <div style={{ display: "flex", flexDirection: "column", alignContent:"flex-start", justifyContent: "flex-start", maxWidth: "90%" }}>
-            <div style={{ maxWidth: "100%" }}>
-                <sp-field-label for="name-0-m">SL</sp-field-label>
-                <sp-textfield style={{ width: "100%" }} id="name-0-m" placeholder="Insira o SL"></sp-textfield>
-            </div>
-            <div style={{ maxWidth: "100%" }}>
-                <sp-field-label for="name-0-m">SSL</sp-field-label>
-                <sp-textfield style={{ width: "100%" }} id="name-0-m" placeholder="Insira o SSL"></sp-textfield>
-            </div>
+        <div>
+            <sp-field-label for="sl-field">SL</sp-field-label>
+            <sp-textfield
+                style={{ width: "100%" }}
+                id="sl-field"
+                placeholder="Insira o SL"
+                value={slValue}
+                onInput={handleSlChange}
+            ></sp-textfield>
+            <sp-field-label for="ssl-field">SSL</sp-field-label>
+            <sp-textfield
+                style={{ width: "100%" }}
+                id="ssl-field"
+                placeholder="Insira o SSL"
+                value={sslValue}
+                onInput={handleSslChange}
+            ></sp-textfield>
         </div>
-    )
+    );
 }
