956ab6cca4c25738a6f7441d83cc4e173a57dd7b
guhesse <hesse.gustavo@gmail.com>
2023-10-29 23:12:19 -0300

    SL and SLL functions are fine, and now we can get height of elements by vars, Headers are fine to

--- src/App.js
+++ src/App.js
@@ -6,8 +6,84 @@ const { core, app } = require('photoshop');
 const { storage } = require('uxp');
 const { batchPlay } = require('photoshop').action;
 
+var slHeight = ""
 
 function App() {
+
+  //Função de moficar e importar o SSL
+
+  const [subjectLineValues, setSubjectLineValues] = useState(null);
+
+  const handleSubjectLineChange = (values) => {
+    setSubjectLineValues(values);
+  };
+
+  const slValue = subjectLineValues?.slValue;
+  const sslValue = subjectLineValues?.sslValue;
+
+  const sslSelect = async () => {
+    const sslFilePath = `assets/NON-LAYOUT_CONTENT.psd`;
+    const fs = storage.localFileSystem;
+
+    try {
+      const pluginDir = await fs.getPluginFolder();
+      const fileEntry = await pluginDir.getEntry(sslFilePath);
+
+
+      const targetFunction = async (executionContext) => {
+        try {
+          await app.open(fileEntry);
+
+          const secondDocument = app.documents[1];
+          const slWidth = secondDocument.width;
+          slHeight = secondDocument.height;
+
+          const batchSSLChangeAndCopy = [
+            { _obj: "select", _target: [{ _ref: "layer", _name: "SL" }], makeVisible: false, layerID: [2125], _options: { dialogOptions: "dontDisplay" } },
+            { _obj: "set", _target: [{ _ref: "textLayer", _enum: "ordinal", _value: "targetEnum" }], to: { _obj: "textLayer", textKey: `SL PT :  ${slValue}`, } },
+            { _obj: "select", _target: [{ _ref: "layer", _name: "SSL" }], makeVisible: false, layerID: [2125], _options: { dialogOptions: "dontDisplay" } },
+            { _obj: "set", _target: [{ _ref: "textLayer", _enum: "ordinal", _value: "targetEnum" }], to: { _obj: "textLayer", textKey: `SSL :  ${sslValue}`, } },
+            { _obj: "selectAllLayers", _target: [{ _ref: "layer", _enum: "ordinal", _value: "targetEnum" }], _options: { dialogOptions: "dontDisplay" } },
+            { _obj: "newPlacedLayer", _options: { dialogOptions: "dontDisplay" } },
+            { _obj: "copyEvent", _options: { dialogOptions: "dontDisplay" } },
+            { _obj: "close", saving: { _enum: "yesNo", _value: "no" }, documentID: 507, _options: { dialogOptions: "dontDisplay" } }
+          ];
+          await batchPlay(batchSSLChangeAndCopy, {});
+
+          const activeDocument = app.activeDocument;
+          await activeDocument.paste();
+
+          console.log("Height SL dentro da função:", slHeight)
+
+
+          const pastedGroup = activeDocument.layers[activeDocument.layers.length - 1];
+          const docWidth = activeDocument.width;
+          const docHeight = activeDocument.height;
+          const offsetX = ((docWidth - docWidth) - (docWidth / 2) + (slWidth / 2));
+          const offsetY = ((docHeight - docHeight) - (docHeight / 2) + (slHeight / 2));
+          pastedGroup.translate(offsetX, offsetY);
+
+          await activeDocument.save();
+
+          console.log('SSL inserido com sucesso!');
+        } catch (error) {
+          console.error('Erro ao inserir SSL:', error);
+        }
+      };
+
+      const options = {
+        commandName: 'Inserir SSL',
+        interactive: true,
+      };
+
+      await core.executeAsModal(targetFunction, options);
+    } catch (error) {
+      console.error('Erro ao encontrar o arquivo de SSL:', error);
+    }
+  };
+
+  // Fim de função de modificar e importar o SSL
+
 
   //Função de selecionar o Header
   const [selectedHeader, setSelectedHeader] = useState(null);
@@ -15,49 +91,41 @@ function App() {
   const handleHeaderSelect = async (header) => {
     const headerFilePath = `assets/${header}.psd`;
     const fs = storage.localFileSystem;
-
     try {
       const pluginDir = await fs.getPluginFolder();
       const fileEntry = await pluginDir.getEntry(headerFilePath);
 
+
       const targetFunction = async (executionContext) => {
         try {
           await app.open(fileEntry);
-          const batchHeaderCopy = [
-            {
-              _obj: "selectAllLayers",
-              _target: [
-                {
-                  _ref: "layer",
-                  _enum: "ordinal",
-                  _value: "targetEnum"
-                }
-              ],
-              _options: {
-                dialogOptions: "dontDisplay"
-              }
-            },
-            {
-              _obj: "copyEvent",
-              _options: {
-                dialogOptions: "dontDisplay"
-              }
-            }
-          ];
+          const secondDocument = app.documents[1];
+          const headerWidth = secondDocument.width;
+          const headerHeight = secondDocument.height;
 
+
+          const batchHeaderCopy = [
+            { _obj: "selectAllLayers", _target: [{ _ref: "layer", _enum: "ordinal", _value: "targetEnum" }], _options: { dialogOptions: "dontDisplay" } },
+            { _obj: "newPlacedLayer", _options: { dialogOptions: "dontDisplay" } },
+            { _obj: "copyEvent", _options: { dialogOptions: "dontDisplay" } },
+            { _obj: "close", saving: { _enum: "yesNo", _value: "no" }, documentID: 507, _options: { dialogOptions: "dontDisplay" } }
+          ];
           await batchPlay(batchHeaderCopy, {});
-          await app.activeDocument.save();
-          await app.activeDocument.close();
 
           const activeDocument = app.activeDocument;
           await activeDocument.paste();
+
           const pastedGroup = activeDocument.layers[activeDocument.layers.length - 1];
           const docWidth = activeDocument.width;
           const docHeight = activeDocument.height;
-          const offsetX = ((docWidth - docWidth) - (docWidth / 2) + 330);
-          const offsetY = ((docHeight - docHeight) - (docHeight / 2) + 57);
+          const offsetX = ((docWidth - docWidth) - (docWidth / 2) + (headerWidth / 2) + 40);
+          const offsetY = ((docHeight - docHeight) - (docHeight / 2) + (headerHeight / 2) + (slHeight + 30));
           pastedGroup.translate(offsetX, offsetY);
+
+
           await activeDocument.save();
+
+
 
           console.log('Header inserido com sucesso!');
         } catch (error) {
@@ -77,151 +145,11 @@ function App() {
   };
   // Fim de função de selecionar o Header
 
-  //Função de moficar e importar o SSL
-
-  const [subjectLineValues, setSubjectLineValues] = useState(null);
-
-  const handleSubjectLineChange = (values) => {
-    setSubjectLineValues(values);
-  };
-
-  const slValue = subjectLineValues?.slValue;
-  const sslValue = subjectLineValues?.sslValue;
-
-  const sslSelect = async () => {
-    const sslFilePath = `assets/NON-LAYOUT_CONTENT.psd`;
-    const fs = storage.localFileSystem;
-
-    try {
-        const pluginDir = await fs.getPluginFolder();
-        const fileEntry = await pluginDir.getEntry(sslFilePath);
-
-        const targetFunction = async (executionContext) => {
-            try {
-                await app.open(fileEntry);
-                const batchSSLChangeAndCopy = [
-                    {
-                        _obj: "select",
-                        _target: [
-                            {
-                                _ref: "layer",
-                                _name: "SL"
-                            }
-                        ],
-                        makeVisible: false,
-                        layerID: [
-                            2125
-                        ],
-                        _options: {
-                            dialogOptions: "dontDisplay"
-                        }
-                    },
-
-                    {
-                        _obj: "set",
-                        _target: [
-                            {
-                                _ref: "textLayer",
-                                _enum: "ordinal",
-                                _value: "targetEnum"
-                            }
-                        ],
-                        to: {
-                            _obj: "textLayer",
-                            textKey: `SL PT :  ${slValue}`,
-                        }
-                    },
-                    {
-                        _obj: "select",
-                        _target: [
-                            {
-                                _ref: "layer",
-                                _name: "SSL"
-                            }
-                        ],
-                        makeVisible: false,
-                        layerID: [
-                            2125
-                        ],
-                        _options: {
-                            dialogOptions: "dontDisplay"
-                        }
-                    },
-
-                    {
-                        _obj: "set",
-                        _target: [
-                            {
-                                _ref: "textLayer",
-                                _enum: "ordinal",
-                                _value: "targetEnum"
-                            }
-                        ],
-                        to: {
-                            _obj: "textLayer",
-                            textKey: `SSL :  ${sslValue}`,
-                        }
-                    },
-                    {
-                        _obj: "selectAllLayers",
-                        _target: [
-                            {
-                                _ref: "layer",
-                                _enum: "ordinal",
-                                _value: "targetEnum"
-                            }
-                        ],
-                        _options: {
-                            dialogOptions: "dontDisplay"
-                        }
-                    },
-                    {
-                        _obj: "copyEvent",
-                        _options: {
-                            dialogOptions: "dontDisplay"
-                        }
-                    }
-                ];
-
-                await batchPlay(batchSSLChangeAndCopy, {});
-                await app.activeDocument.save();
-                await app.activeDocument.close();
-
-                const activeDocument = app.activeDocument;
-                await activeDocument.paste();
-                const pastedGroup = activeDocument.layers[activeDocument.layers.length - 1];
-                const docWidth = activeDocument.width;
-                const docHeight = activeDocument.height;
-                const offsetX = ((docWidth - docWidth) - (docWidth / 2) + 325);
-                const offsetY = ((docHeight - docHeight) - (docHeight / 2) + 30);
-                pastedGroup.translate(offsetX, offsetY);
-                await activeDocument.save();
-
-                console.log('SSL inserido com sucesso!');
-            } catch (error) {
-                console.error('Erro ao inserir SSL:', error);
-            }
-        };
-
-        const options = {
-            commandName: 'Inserir SSL',
-            interactive: true,
-        };
-
-        await core.executeAsModal(targetFunction, options);
-    } catch (error) {
-        console.error('Erro ao encontrar o arquivo de SSL:', error);
-    }
-};
-
-
-  
-  // Fim de função de modificar e importar o SSL
 
   const handleMontarLayoutClick = async () => {
     try {
-      await sslSelect();
-      await handleHeaderSelect(selectedHeader);
+      var slHeight = await sslSelect(); // Obtém slHeight usando sslSelect
+      await handleHeaderSelect(selectedHeader, slHeight); // Passa slHeight para handleHeaderSelect
       // Outras funções que você deseja executar podem ser chamadas aqui
       // await outraFuncao();
       // await maisUmaFuncao();
